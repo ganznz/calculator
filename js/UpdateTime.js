3f97d8150eb40b9e51506fcebc3f85f4bcdc67c8
Ganz <loganbellinghamlb@gmail.com>
2022-02-01 12:16:00 +1300

    Fixed time display not showing correct date format if minutes less than 10

--- js/UpdateTime.js
+++ js/UpdateTime.js
@@ -6,8 +6,10 @@ timeDisplayText.textContent = `${dateHours.toString()}:${dateMins.toString()}`;
 
 const changeTime = () => {
     dateHours = new Date().getHours();
-    dateMins = new Date().getMinutes();
-    timeDisplayText.textContent = `${dateHours.toString()}:${dateMins.toString()}`;
+    dateMins = new Date().getMinutes()
+
+    dateMins < 10 ? timeDisplayText.textContent = `${dateHours.toString()}:0${dateMins.toString()}`
+    : timeDisplayText.textContent = `${dateHours.toString()}:${dateMins.toString()}`;
 }
 
 setInterval(changeTime, 1000);
